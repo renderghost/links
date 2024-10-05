b1cb87c4b71863755ff7bea8f4f667f72ae03194
Barry Prendergast <barry.m.prendergast@gmail.com>
2024-10-05 19:24:40 +0200

    feat(avatar): update avatar styling
    Updating the avatar-work.svg file to change the background color from black to a deeper purple color (#190C4F). This change is part of the larger effort to refine the visual design of the application.

---
+++ src/components/Creations.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import React from 'react';
+import { FilledButton } from './atom/Button';
+import Section from './atom/Section';
+import { Aperture, ExternalLink, Github, ListMusic } from 'lucide-react';
+
+interface Creation {
+	title: string;
+	ariaLabel: string;
+	icon: React.FC<React.SVGProps<SVGSVGElement>>;
+	url: string;
+	sourceUrl?: string;
+}
+
+const creationsList: Creation[] = [
+	{
+		title: 'Aperture',
+		ariaLabel:
+			'My amatuer snaps gallery where I pretend to be a pro photographer.',
+		icon: Aperture,
+		url: 'https://frame.renderg.host/',
+		sourceUrl: 'https://github.com/renderghost/frame',
+	},
+	{
+		title: 'Selections',
+		ariaLabel:
+			'A fancy discovery app for my many many Spotify playlists. Warning: May contain questionable music choices!',
+		icon: ListMusic,
+		url: 'https://selections.renderg.host/',
+		sourceUrl: 'https://github.com/renderghost/selections',
+	},
+];
+
+const CreationCard: React.FC<Creation> = ({
+	title,
+	ariaLabel,
+	icon: Icon,
+	url,
+	sourceUrl,
+}) => (
+	<div className='bg-neutral-100 dark:bg-brand-900 flex flex-col justify-between gap-8 p-8'>
+		<div className='flex flex-col gap-2'>
+			<div className='flex items-center gap-2'>
+				<Icon className='w-7 h-' />
+				<h3 className='font-medium text-xl'>{title}</h3>
+			</div>
+			<p className=' text-brand-900 dark:text-brand-100'>{ariaLabel}</p>
+		</div>
+		<div className='block'>
+			<div className='flex flex-wrap gap-3'>
+				<FilledButton
+					variant='secondary'
+					RightIcon={ExternalLink}
+					onClick={() => window.open(url, '_blank')}
+					ariaLabel={`View ${title} creation`}
+					tooltip={`View ${title} creation`}
+				>
+					View Creation
+				</FilledButton>
+				{sourceUrl && (
+					<FilledButton
+						variant='transparent'
+						RightIcon={Github}
+						onClick={() => window.open(sourceUrl, '_blank')}
+						ariaLabel={`View source code for ${title}`}
+						tooltip={`View source code for ${title}`}
+					>
+						View Source
+					</FilledButton>
+				)}
+			</div>
+		</div>
+	</div>
+);
+
+const Creations: React.FC = () => (
+	<Section
+		id='my-creations'
+		title='Creations'
+		description='Artistic works here and there.'
+	>
+		<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4'>
+			{creationsList.map((creation, index) => (
+				<CreationCard key={index} {...creation} />
+			))}
+		</div>
+	</Section>
+);
+
+export default Creations;
--- src/components/atom/Section.tsx
+++ src/components/atom/Section.tsx
@@ -5,18 +5,23 @@ import React from 'react';
 interface SectionProps {
 	id: string;
 	title: string;
+	description: string;
 	children: React.ReactNode;
 	className?: string;
 }
 
 // Section component, reusable for different parts of the page
-const Section: React.FC<SectionProps> = (
-	{ id, title, children }, //Add 'title' to ({ id, title, children }) to restore
-) => (
-	<section id={id} className='flex flex-col gap-4'>
-		<h2 className='text-3xl font-semibold text-brand-950 dark:text-neutral-100'>
-			{title}
-		</h2>
+const Section: React.FC<SectionProps> = ({
+	id,
+	title,
+	description,
+	children,
+}) => (
+	<section id={id} className='flex flex-col gap-6'>
+		<div className='flex flex-col gap-1 text-brand-950 dark:text-neutral-100'>
+			<h2 className='text-2xl font-semibold'>{title}</h2>
+			<p className=''>{description}</p>
+		</div>
 		{children}
 	</section>
 );
