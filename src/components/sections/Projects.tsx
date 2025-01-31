967055bf9dcdc15301b59cf6a5a59c05fc50c4c9
Barry Prendergast <barry.m.prendergast@gmail.com>
2025-01-31 12:09:15 +0100

    refactor: update section descriptions for clarity and consistency

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -120,7 +120,7 @@ const Projects: React.FC = () => (
 	<Section
 		id='my-lab'
 		title='Design Tools'
-		description='Learning through playing with constantly-breaking code.'
+		description='Links to things I made for work.'
 	>
 		<div
 			className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-1'
